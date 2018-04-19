c9fb904e53674bccb79967dd7d31f46f922bafe1
Aaron Chang <aaron.chang@quadas.com>
2018-04-19 20:39:33 +0800

    add error response

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,12 +12,27 @@ const db = {
 const queryStr =
   "SELECT * FROM blockchain.transaction INNER JOIN blockchain.asset ON transaction.tx_asset_id = asset.asset_id INNER JOIN blockchain.block ON block.block_number = asset.asset_block_number ORDER BY tx_modified_at LIMIT 1;";
 
+// response
+let resposne = {
+  status: 200,
+  data: [],
+  message: null
+};
+
+// error response
+const sendError = (err, res) => {
+  response.status = 501;
+  response.message = typeof err == "object" ? err.message : err;
+  res.status(501).join(response);
+};
+
 // connect to psql
 const connect = query => {
   const client = new Client(db);
 
   client.connect(err => {
     if (err) {
+      sendError(err, res);
       console.log("connection error", err.stack);
     } else {
       query(client);
