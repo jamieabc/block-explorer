845a9d9a2797a8dce78c0bf85381d3a316f051d7
Aaron Chang <aaron.chang@quadas.com>
2018-04-23 13:02:25 +0800

    add e2e test of webpage

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,28 @@
 import { AppPage } from './app.po';
 
 describe('block-explorer App', () => {
-  let page: AppPage;
+    let page: AppPage;
+    let defaultData: {
+        block_number: string;
+        block_hash: string;
+        timestamp: string;
+        tx_id: string;
+    } = {
+        block_number: '2',
+        block_hash: '00a9ff09086a66ad85b082e9f8f5b92d1c103ffedc6b61934a93a367bd4829b6',
+        timestamp: 'Apr 10, 2018 03:36:53 PM',
+        tx_id: 'd8b920f7d2925e74d2038d817118de3701bf980ce8f5e2e79d041bb283a6be59'
+    };
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    it('should display correct page', () => {
+        page.waitForRootReady();
+        expect(page.getBlockNumberText()).toEqual(`Block #${defaultData.block_number}`);
+        expect(page.getBlockHashText()).toEqual(defaultData.block_hash);
+        expect(page.getBlockTimestampText()).toEqual(defaultData.timestamp);
+        expect(page.getBlockTxIdText()).toEqual(defaultData.tx_id);
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,26 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    getBlockNumberText() {
+        return element(by.id('block-number')).getText();
+    }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getBlockHashText() {
+        return element(by.id('block-hash')).getText();
+    }
+
+    getBlockTimestampText() {
+        return element(by.id('block-timestamp')).getText();
+    }
+
+    getBlockTxIdText() {
+        return element(by.id('block-tx-id')).getText();
+    }
+
+    waitForRootReady() {
+        browser.get('/');
+        browser.wait(() => {
+            return element(by.id('wrap')).isPresent();
+        }, 10000);
+    }
 }
