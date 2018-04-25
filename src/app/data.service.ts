47682a19bedc9888fea8d4ae153df1afe1dd83b9
Aaron Chang <aaron.chang@quadas.com>
2018-04-25 23:11:41 +0800

    add function to query incremently

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -18,7 +18,7 @@ export class DataService {
     constructor(private _http: Http) {}
 
     getData(parsedUrl: UrlModel) {
-        const { pathname } = parsedUrl;
+        const { hostname, pathname } = parsedUrl;
         const [ prefix, category, resourceId, indicator ] = pathname.split('/');
         let transaction;
 
@@ -26,24 +26,39 @@ export class DataService {
             // get info by asset id
             transaction =
                 this._http
-                .get(`http://${parsedUrl.hostname}:3000/api/asset/${resourceId}`);
+                .get(`http://${hostname}:3000/api/asset/${resourceId}`);
         } else if (category === 'block') {
             // get transaction(s) in a block
             transaction =
                 this._http
-                .get(`http://${parsedUrl.hostname}:3000/api/block/${resourceId}/${indicator}`);
+                .get(`http://${hostname}:3000/api/block/${resourceId}/${indicator}`);
 
         } else if (pathname.match(/^\/transaction\/\d+/)) {
             // get info by transaction id
             transaction =
                 this._http
-                .get(`http://${parsedUrl.hostname}:3000/api/transaction/${resourceId}`);
+                .get(`http://${hostname}:3000/api/transaction/${resourceId}`);
         } else {
             // get transaction of latest block
             transaction =
                 this._http
-                .get(`http://${parsedUrl.hostname}:3000/api/block/transactions`);
+                .get(`http://${hostname}:3000/api/block/transactions`);
         }
+
+        return transaction.map(data => this.result = data.json());
+    }
+
+    getMoreTransactions(parsedUrl: UrlModel, offset: number, limit: number, blockNumber: string) {
+        const { hostname, pathname } = parsedUrl;
+        const [ prefix, category, resourceId, indicator ] = pathname.split('/');
+
+        const host = `http://${hostname}:3000`;
+        const urlPath = `/api/block/${resourceId ? resourceId : blockNumber}/transactions`;
+        const query = `?limit=${limit}&offset=${offset}`;
+
+        const transaction =
+            this._http
+            .get(host + urlPath + query);
 
         return transaction.map(data => this.result = data.json());
     }
